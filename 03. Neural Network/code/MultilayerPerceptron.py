491ce49a733b25764508c2551517f3a7181ea7ea
Miroslav Dzhokanov <mdzhokanov@gmail.com>
2025-06-19 10:30:14 +0300

    Add comments

--- 03. Neural Network/code/MultilayerPerceptron.py
+++ 03. Neural Network/code/MultilayerPerceptron.py
@@ -2,67 +2,170 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class MultilayerPerceptron:
+    '''
+    Multilayer Perceptron class to train a multilayer perceptron model with three layers: input, hidden and output.
+    '''
+    # Attributes
+    weights: list[np.ndarray]
+    biases: list[np.ndarray]
+
+    # Constructor
     def __init__(self, input_size: int, hidden_size: int, output_size: int):
-        # Initialize weights and biases using random numbers
+        '''
+        Initialize the Multilayer Perceptron object.
+
+        Args:
+            input_size          : int - number of input nodes
+            hidden_size         : int - number of hidden nodes
+            output_size         : int - number of output nodes
+        '''
+
+        # Initialize weights using random numbers
         self.weights = [np.random.randn(hidden_size, input_size), np.random.randn(output_size, hidden_size)]
+        # Initialize biases to 0
         self.biases = [np.zeros((hidden_size, 1)), np.zeros((output_size, 1))]
 
-    def sigmoid(self, X: np.ndarray) -> np.ndarray:
-        return 1 / (1 + np.exp(-X))
+    # Activation function
+    def sigmoid(self, Z: np.ndarray) -> np.ndarray:
+        '''
+        Calculate the sigmoid of the weighted input.
 
-    def sigmoid_derivative(self, X: np.ndarray) -> np.ndarray:
-        return X * (1 - X)
+        Args:
+            Z                   : np.ndarray - input with shape (layer_size, m), where m is the number of observations
 
-    def forward(self, X: np.ndarray, print_values: bool = False) -> list[np.ndarray]:
+        Returns:
+            sigmoid             : np.ndarray - sigmoid of the input with shape (layer_size, m)
+        '''
+        return 1 / (1 + np.exp(-Z))
+
+    # Activation function derivative
+    def sigmoid_derivative(self, Z: np.ndarray) -> np.ndarray:
+        '''
+        Calculate the derivative of the sigmoid function.
+
+        Args:
+            Z                   : np.ndarray - input with shape (layer_size, m), where m is the number of observations
+
+        Returns:
+            sigmoid_derivative  : np.ndarray - derivative of the sigmoid function with shape (layer_size, m)
+        '''
+        return Z * (1 - Z)
+
+    def forward(self, X: np.ndarray) -> list[np.ndarray]:
+        '''
+        Forward pass through the network.
+
+        Args:
+            X                   : np.ndarray - input with shape (m, n), where m is the number of observations 
+                                               and n is the number of features (input_size)
+
+        Returns:
+            a1                  : np.ndarray - activation with shape (hidden_size, m)
+            a2                  : np.ndarray - activation with shape (output_size, m)
+        '''
+        # z1 = w1 * X.T + b1
+        # z1 = (hidden_size, input_size) * (input_size, m) + (hidden_size, 1)
+        # z1 = (hidden_size, m)
         z1 = self.weights[0] @ X.T + self.biases[0]
+        # The shape of a1 is (hidden_size, m).
         a1 = self.sigmoid(z1)
 
+        # z2 = w2 * a1 + b2
+        # z2 = (output_size, hidden_size) * (hidden_size, m) + (output_size, 1)
         z2 = self.weights[1] @ a1 + self.biases[1]
+        # The shape of a2 is (output_size, m).
         a2 = self.sigmoid(z2)
 
-        return [z1, z2], [a1, a2]
+        return [a1, a2]
 
-    def backward(self, X: np.ndarray, Y: np.ndarray, a: list[np.ndarray]):
-        a1 = a[0]
-        a2 = a[1]
+    def backward(self, X: np.ndarray, Y: np.ndarray, activations: list[np.ndarray]) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        '''
+        Backward pass through the network. Here is where the magic happens.
+
+        Args:
+            X                   : np.ndarray - input with shape (m, n), where m is the number of observations 
+                                               and n is the number of features (input_size)
+            Y                   : np.ndarray - output with shape (m, 1)
+            activations         : list[np.ndarray] - list of activations with shape (layer_size, m)
+
+        Returns:
+            dc_dw1              : np.ndarray - derivative of the error w.r.t. the weights of the hidden layer with shape (hidden_size, input_size)
+            dc_dw2              : np.ndarray - derivative of the error w.r.t. the weights of the output layer with shape (output_size, hidden_size)
+            dc_db1              : np.ndarray - derivative of the error w.r.t. the biases of the hidden layer with shape (hidden_size, 1)
+            dc_db2              : np.ndarray - derivative of the error w.r.t. the biases of the output layer with shape (output_size, 1)
+        '''
+        a1 = activations[0] # The shape of a1 is (hidden_size, m).
+        a2 = activations[1] # The shape of a2 is (output_size, m).
 
         # Calculate the error
-        error = (a2 - Y.T) ** 2
-        n = len(Y)
+        error = (a2 - Y.T) ** 2 # The shape of error is (output_size, m) and the shape of Y.T is (output_size, m)
+        m = len(Y) # The number of observations
 
+        # The shape of dc_da2 is (output_size, m)
         dc_da2 = 2 * (a2 - Y.T)                             # Derivative of the error w.r.t. the activation
+        # The shape of dc_dz2 is (output_size, m)
         dc_dz2 = self.sigmoid_derivative(a2) * dc_da2       # Derivative of the error w.r.t. the delta of the output layer
-        dc_dw2 = np.dot(dc_dz2, a1.T) / n                   # Derivative of the error w.r.t. the weights of the output layer
-        dc_db2 = np.sum(dc_dz2, axis=1, keepdims=True) / n  # Derivative of the error w.r.t. the biases of the output layer
+        # The shape of dc_dw2 is (output_size, hidden_size)
+        dc_dw2 = np.dot(dc_dz2, a1.T) / m                   # Derivative of the error w.r.t. the weights of the output layer
+        # The shape of dc_db2 is (output_size, 1)
+        dc_db2 = np.sum(dc_dz2, axis=1, keepdims=True) / m  # Derivative of the error w.r.t. the biases of the output layer
 
+        # The shape of dc_da1 is (hidden_size, m)
         dc_da1 = self.weights[1].T @ dc_dz2                 # Derivative of the error w.r.t. the activation of the hidden layer
+        # The shape of dc_dz1 is (hidden_size, m)
         dc_dz1 = self.sigmoid_derivative(a1) * dc_da1       # Derivative of the error w.r.t. the delta of the hidden layer
-        dc_dw1 = np.dot(dc_dz1, X) / n                      # Derivative of the error w.r.t. the weights of the hidden layer
-        dc_db1 = np.sum(dc_dz1, axis=1, keepdims=True) / n  # Derivative of the error w.r.t. the biases of the hidden layer
+        # The shape of dc_dw1 is (hidden_size, input_size)
+        dc_dw1 = np.dot(dc_dz1, X) / m                      # Derivative of the error w.r.t. the weights of the hidden layer
+        # The shape of dc_db1 is (hidden_size, 1)
+        dc_db1 = np.sum(dc_dz1, axis=1, keepdims=True) / m  # Derivative of the error w.r.t. the biases of the hidden layer
 
         return dc_dw1, dc_dw2, dc_db1, dc_db2, error
 
-    def update_weights_and_biases(self, dc_dw1: np.ndarray, dc_dw2: np.ndarray, dc_db1: np.ndarray, dc_db2: np.ndarray, learning_rate: float):
-        self.weights[0] -= learning_rate * dc_dw1
-        self.weights[1] -= learning_rate * dc_dw2
-        self.biases[0] -= learning_rate * dc_db1
-        self.biases[1] -= learning_rate * dc_db2
+    def update_weights_and_biases(self, dc_dw1: np.ndarray, dc_dw2: np.ndarray, dc_db1: np.ndarray, dc_db2: np.ndarray, learn_rate: float):
+        '''
+        Update the weights and biases of the network. The learning rate is a hyperparameter that controls the step size of the gradient descent.
+
+        Args:
+            dc_dw1              : np.ndarray - derivative of the error w.r.t. the weights of the hidden layer with shape (hidden_size, input_size)
+            dc_dw2              : np.ndarray - derivative of the error w.r.t. the weights of the output layer with shape (output_size, hidden_size)
+            dc_db1              : np.ndarray - derivative of the error w.r.t. the biases of the hidden layer with shape (hidden_size, 1)
+            dc_db2              : np.ndarray - derivative of the error w.r.t. the biases of the output layer with shape (output_size, 1)
+            learn_rate          : float - learning rate
+        '''
+        self.weights[0] -= learn_rate * dc_dw1
+        self.weights[1] -= learn_rate * dc_dw2
+        self.biases[0] -= learn_rate * dc_db1
+        self.biases[1] -= learn_rate * dc_db2
     
-    def train(self, X: np.ndarray, Y: np.ndarray, learning_rate: float, epochs: int):
+    def train(self, X: np.ndarray, Y: np.ndarray, learn_rate: float, epochs: int) -> list[float]:
+        '''
+        Train the network.
+
+        Args:
+            X                   : np.ndarray - input with shape (m, n), where m is the number of observations 
+                                               and n is the number of features (input_size)
+            Y                   : np.ndarray - output with shape (m, 1)
+            learn_rate          : float - learning rate
+            epochs              : int - number of epochs
+
+        Returns:
+            losses              : list[float] - list of losses for each epoch
+        '''
         losses = []
         for epoch in range(epochs):
-            z, a = self.forward(X)
+            activations = self.forward(X)
 
-            dc_dw1, dc_dw2, dc_db1, dc_db2, error = self.backward(X, Y, a)
+            dc_dw1, dc_dw2, dc_db1, dc_db2, error = self.backward(X, Y, activations)
             losses.append(np.sum(error))
 
             # Update the weights and biases
-            self.update_weights_and_biases(dc_dw1, dc_dw2, dc_db1, dc_db2, learning_rate)
+            self.update_weights_and_biases(dc_dw1, dc_dw2, dc_db1, dc_db2, learn_rate)
 
             if epoch % 1000 == 0:
                 print(f"Epoch {epoch} error: {round(np.sum(error), 3)}")
         return losses
 
+# Demo of training the Multilayer Perceptron model with data about salaries, years of experience and level of education
 def main():
     # X1 = years of experience
     X1 = [1.2, 1.3, 1.5, 1.8, 2, 2.1, 2.2, 2.5, 2.8, 2.9, 3.1, 3.3, 3.5, 3.8, 4, 4.1, 4.5, 4.9, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 10, 11, 12, 13, 14, 15]
--- 03. Neural Network/code/Perceptron.py
+++ 03. Neural Network/code/Perceptron.py
@@ -1,46 +1,121 @@
 import numpy as np
 
 class Perceptron:
-    learning_rate: float
-    n_iterations: int
-    w: np.ndarray
-    b: float
+    '''
+    Perceptron class to train a perceptron model.
+    '''
 
+    # Attributes
+    learning_rate   : float
+    n_iterations    : int
+    w               : np.ndarray
+    b               : float
+
+    # Constructor
     def __init__(self, learning_rate=0.01, n_iterations=1000):
+        '''
+        Initialize the Perceptron object.
+
+        Args:
+            learning_rate       : float - learning rate
+            n_iterations        : int - number of iterations (epochs)
+        '''
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
         self.w = np.zeros(2)
         self.b = 0
 
-    def weighted_sum(self, X: np.ndarray) -> float:
-        return np.dot(X, self.w) + self.b
+    def weighted_sum(self, x: np.ndarray) -> float:
+        '''
+        Calculate the weighted sum of the input features - x_1 * w_1 + x_2 * w_2 + ... + x_n * w_n + b.
+
+        Args:
+            x                   : np.ndarray - a single observation with shape (n, )
+
+        Returns:
+            weighted_sum        : float - weighted sum
+        '''
+        return np.dot(x, self.w) + self.b
     
-    def predict(self, X: np.ndarray) -> float:
-        H = self.weighted_sum(X)
+    def predict(self, x: np.ndarray) -> float:
+        '''
+        Predict the binary output for single observation.
+
+        Args:
+            x                   : np.ndarray - a single observation with shape (n, )
+
+        Returns:
+            y_pred              : float - predicted output (0 or 1)
+        '''
+        H = self.weighted_sum(x)
         return np.where(H >= 0.5, 1, 0) # Binary classification (0 or 1)
     
     def calculate_error(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        '''
+        Calculate the error between the true and predicted output and multiply it by the learning rate.
+
+        Args:
+            y_true              : float - true output
+            y_pred              : float - predicted output
+
+        Returns:
+            error               : float - error (learn_rate * (y_true - y_pred))
+        '''
         return self.learning_rate * (y_true - y_pred)
     
-    def update_weights(self, X: np.ndarray, error: float) -> None:
-        self.w += error * X
+    def update_weights(self, x: np.ndarray, error: float) -> None:
+        '''
+        Update the weights (w_1, w_2, ..., w_n) of the perceptron by adding the error multiplied by the observation.
+
+        Args:
+            x                   : np.ndarray - a single observation with shape (n, )
+            error               : float - error (learn_rate * (y_true - y_pred))
+
+        Returns:
+            None
+        '''
+        self.w += error * x # The derivative of the error w.r.t. the weights is (y_true - y_pred) * x
     
     def update_bias(self, error: float) -> None:
-        self.b += error
+        '''
+        Update the bias of the perceptron by adding the error.
+
+        Args:
+            error               : float - error (y_true - y_pred)
+
+        Returns:
+            None
+        '''
+        self.b += error # The derivative of the error w.r.t. the bias is (y_true - y_pred)
     
     def train(self, X: np.ndarray, Y: np.ndarray) -> tuple[np.ndarray, float]:
+        '''
+        Train the perceptron by updating the weights and bias for each observation in the dataset.
+
+        Args:
+            X                   : np.ndarray - a dataset with shape (m, n)
+            Y                   : np.ndarray - a vector of true outputs with shape (m, )
+
+        Returns:
+            w                   : np.ndarray - final weights with shape (n, )
+            b                   : float - final bias
+        '''
         for _ in range(self.n_iterations):
             total_error = 0
             for i in range(len(X)):
+                # STEP 1: Predict the output for the current observation
                 y_pred = self.predict(X[i])
+                # STEP 2: Calculate the error between the true and predicted output
                 error = self.calculate_error(Y[i], y_pred)
+                # STEP 3: Update the weights and bias
                 self.update_weights(X[i], error)
                 self.update_bias(error)
-            total_error += error
+                # STEP 4: Accumulate the total error
+                total_error += error
             print(f"Epoch #{_ + 1} - Total Error: {total_error}")
         return self.w, self.b
     
-# Little demo
+# Little demo for AND gate problem - 00, 01, 10, 11 -> 0, 0, 0, 1
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 Y = np.array([0, 0, 0, 1])
 
